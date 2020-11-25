187336615ecca233b83e6dd6e07a4fd2ca7524cf
Jonathan Queiroz <dev@johnjq.com>
2020-11-25 08:55:15 -0800

    Rename: InteractiveDictionary -> Glosser

--- eberlib/src/main.rs
+++ eberlib/src/main.rs
@@ -5,7 +5,7 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(about = "the official eberban command-line utilities")]
 enum Opt {
-    InteractiveDictionary {
+    Glosser {
         /// Dictionary filename
         #[structopt(short, long, parse(from_os_str))]
         dictionary_filename: PathBuf,
@@ -16,7 +16,7 @@ fn main() {
     let opt = Opt::from_args();
 
     match opt {
-        Opt::InteractiveDictionary { dictionary_filename } => {
+        Opt::Glosser { dictionary_filename } => {
             let file = File::open(&dictionary_filename).unwrap_or_else(|error| {
                 panic!("Unable to open dictionary file {:?} -- {:?}", dictionary_filename, error);
             });
