a425cedc6f6a9cbebc2f963d6a4d0e9f02e4ea49
Jonathan Queiroz <dev@johnjq.com>
2020-11-26 09:15:07 -0800

    Remove sample tests

--- eberlib/src/lib.rs
+++ eberlib/src/lib.rs
@@ -1,11 +1,3 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
 pub mod dict {
     use std::collections::BTreeMap;
     use std::{fs::File,io::BufReader};
