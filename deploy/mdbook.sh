a7aab711922a27f48ae3959c3fb50e4ec655ce7b
Jonathan Queiroz <dev@johnjq.com>
2020-11-27 09:26:24 -0800

    Attempt to fix deployment

--- deploy/mdbook.sh
+++ deploy/mdbook.sh
@@ -7,4 +7,5 @@ rm -f books/tour/.gitignore
 git checkout -b tmp/gh-pages
 git add -A
 git commit -m "deployed on $(date) by ${USER}"
-git push origin tmp/gh-pages:gh-pages --force
+#git push origin tmp/gh-pages:gh-pages --force
+git push https://mia-entropy:$GITHUB_TOKEN@github.com/mia-entropy/eberban.git master
