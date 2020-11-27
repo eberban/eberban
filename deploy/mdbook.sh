d4b4dad7278fb37e79686dba1e6103db1f253eb4
Jonathan Queiroz <dev@johnjq.com>
2020-11-27 08:59:42 -0800

    Fix deployment

--- deploy/mdbook.sh
+++ deploy/mdbook.sh
@@ -4,7 +4,7 @@ set -e
 echo "====> deploying to github"
 
 rm -f books/tour/.gitignore
-git checkout -b gh-pages/tmp
+git checkout -b tmp/gh-pages
 git add -A
 git commit -m "deployed on $(shell date) by ${USER}"
 git push origin gh-pages --force
