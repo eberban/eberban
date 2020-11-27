6d26295f3387b2aaa88e5241d405b1105e73cd2c
Jonathan Queiroz <dev@johnjq.com>
2020-11-27 09:39:12 -0800

    Fix deployment script

--- deploy/mdbook.sh
+++ deploy/mdbook.sh
@@ -8,4 +8,4 @@ git checkout -b tmp/gh-pages
 git add -A
 git commit -m "deployed on $(date) by ${USER}"
 #git push origin tmp/gh-pages:gh-pages --force
-git push https://mia-entropy:$GITHUB_TOKEN@github.com/mia-entropy/eberban.git master
+git push https://mia-entropy:$GITHUB_TOKEN@github.com/mia-entropy/eberban.git tmp/gh-pages:gh-pages
