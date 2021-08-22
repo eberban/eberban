e95f937bc7450a4bafd54925c98e5068cd576588
mia entropy <63742169+mia-entropy@users.noreply.github.com>
2021-08-22 15:42:33 +0200

    glosses notation + SE explanation

--- books/tour_en/res/leipzig_apply.js
+++ books/tour_en/res/leipzig_apply.js
@@ -1,3 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 	Leipzig({ abbreviations: {}, firstLineOrig: true}).gloss();
 });
+
+document.addEventListener('gloss:complete', function(event) {
+	console.log({document});
+
+	let element = document.documentElement;
+
+	// element.innerHTML = element.innerHTML.replaceAll(/\$\$\{(.+)\}/g, '<span class="cursive">$1</span>');
+	element.innerHTML = element.innerHTML.replaceAll(/\$\((.+?)\)/g, '<span class="cursive">$1</span>');
+});
+
+// MathJax.Hub.Config({
+// 	tex2jax: {
+// 	 inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+// 	 processEscapes: true
+// 	}
+// });
