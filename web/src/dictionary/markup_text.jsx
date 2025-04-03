54461484a4a814b9e8f79c1a0a08820912f6b672
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-04-04 07:00:51 +1100

    ✨-unintegrated Add new dictionary rendering code
    This change is unintegrated and thus has no effect on the website.
    
    It's my view that the current implementation to render dictionary entries
    unreadable:
    
    - It uses raw RegEx which is [too terse to be readable][link]
    - It uses HTML in strings which loses HTML syntax highlighting
    
    This change (let's call it "the kit method") aims to be scaleable as well
    as readable.
    
    Kits (a kit is a regex pattern and corresponding replacer function) can
    be easily added, nested, and removed from the list that the markup
    function uses.
    
    [link]: https://softwareengineering.stackexchange.com/a/298575

---
+++ web/src/dictionary/markup_text.jsx
@@ -0,0 +1,160 @@
+import reactStringReplace from "react-string-replace";
+import {
+    any_of,
+    any_number_of,
+    backslash,
+    does_not_begin_with,
+    fewest_positive_number_of,
+    group,
+    line_feed,
+    non_capturing_group,
+    one_or_more,
+    optional,
+} from "../scripts/regex";
+
+
+/* HELPERS */
+
+
+const remove_delimiters = (text) => text.substring(1, text.length - 1);
+
+function DictLink({ children }) {
+    return <a href={`#${children}`}>{children}</a>;
+}
+
+
+/* 
+    Kit :: () -> { regex: RegExp(), replacer: string -> JSX }
+
+    Kits are how we extend markup syntax. They're written as functions for
+    readability.
+*/
+
+
+function break_kit() {
+    return {
+        regex: new RegExp(group(backslash + line_feed), "g"),
+        replacer: () => <br />,
+    };
+}
+
+function bold_kit() {
+    const bolded = (s) => "__" + s + "__";
+    return {
+        regex: new RegExp(group(bolded(fewest_positive_number_of("."))), "g"),
+        replacer: (s) => <strong>{remove_delimiters(remove_delimiters(s))}</strong>,
+    };
+}
+
+function italics_kit() {
+    const in_italics = (s) => "_" + s + "_";
+    return {
+        regex: new RegExp(group(in_italics(fewest_positive_number_of("."))), "g"),
+        replacer: (s) => <em>{remove_delimiters(s)}</em>,
+    };
+}
+
+function definition_quote_kit() {
+    const in_quote = (s) => "`" + s + "`";
+    return {
+        regex: new RegExp(group(in_quote(fewest_positive_number_of("."))), "g"),
+        replacer: (s) => <code>{remove_delimiters(s)}</code>,
+    };
+}
+
+function eberban_quote_kit() {
+    function whole() {
+        const in_quote = (s) => "{" + s + "}";
+        return {
+            regex: new RegExp(group(in_quote(fewest_positive_number_of("."))), "g"),
+            replacer: (string) => {
+                const kit = content_kit();
+                let content = remove_delimiters(string);
+                content = reactStringReplace(content, kit.regex, kit.replacer);
+                content = reactStringReplace(content, "!", () => "");
+                return <span class="ebb-quote">{content}</span>;
+            },
+        };
+    };
+    function content_kit() {
+        const as_one_link = (s) => "<" + s + ">";
+        const simple_word_link = 
+            does_not_begin_with("!" + any_number_of("\\w")) + one_or_more("\\w");
+        const compound_word_link = as_one_link(non_capturing_group(
+            fewest_positive_number_of(
+                non_capturing_group(one_or_more("\\w") + optional("\\s")),
+            ),
+        ));
+        return {
+            regex: new RegExp(group(any_of(simple_word_link, compound_word_link)), "g"),
+            replacer: (string) => {
+                let output = string;
+                if (new RegExp(compound_word_link).test(output)) {
+                    output = remove_delimiters(output);
+                }
+                return <DictLink>{output}</DictLink>;
+            }
+        };
+    };
+    return whole();
+}
+
+function place_kit() {
+    function whole() {
+        const in_brackets = (s) => "\\[" + s + "\\]";
+        const place = non_capturing_group(any_of("E", "A", "O", "U"));
+        const arg = non_capturing_group(":" + fewest_positive_number_of("."));
+        return {
+            regex: new RegExp(group(in_brackets(place + optional(arg))), "g"),
+            replacer: (string) => {
+                const kit = content_kit();
+                const [left_bracket, content, right_bracket] = [
+                    string.slice(0, 1),
+                    reactStringReplace(string.slice(1, -1), kit.regex, kit.replacer),
+                    string.slice(-1),
+                ];
+                return (
+                    <span class="label label-info place">
+                        <span class="hidden">{left_bracket}</span>
+                        {content}
+                        <span class="hidden">{right_bracket}</span>
+                    </span>
+                );
+            },
+        };
+    };
+    function content_kit() {
+        return {
+            regex: new RegExp(group("\\w" + one_or_more("\\w")), "g"),
+            replacer: (s) => <DictLink>{s}</DictLink>,
+        };
+    };
+    return whole();
+}
+
+
+/* ENTRY */
+
+
+export function markup_inline(text) {
+    let output = text;
+    const markup_kits = [
+        break_kit,
+        bold_kit, // bold before italics otherwise ___s___ ->  _<em>{s}</em>_
+        italics_kit,
+        definition_quote_kit,
+        eberban_quote_kit,
+        place_kit,
+    ];
+    for (const kit of markup_kits) {
+        const { regex, replacer } = kit();
+        output = reactStringReplace(output, regex, replacer);
+    }
+    return output;
+}
+
+export function markup_block(text) {
+    return text
+        .split(new RegExp(group(line_feed + line_feed), "g"))
+        .map((content) => <p>{markup_inline(content)}</p>);    
+}
--- web/src/scripts/regex.js
+++ web/src/scripts/regex.js
@@ -1,17 +1,33 @@
+/* GROUPS */
+
+export const group = (s) => "(" + s + ")";
+export const non_capturing_group = (s) => "(?:" + s + ")";
+
+
+/* LOOKAROUNDS */
+
+export const does_not_begin_with = (s) => "(?<!" + s + ")";
+
+
+/* QUANTIFIERS & ALTERNATION */
+
+export const any_of = (...strings) =>  strings.join("|");
+
+export const any_number_of = (s) => s + "*";
+export const one_or_more = (s) => s + "+";
+
+export const fewest_positive_number_of = (s) => s + "+?";
+
+export const optional = (s) => s + "?";
+
+
 /* CHARACTERS & SETS */
+
+export const ignore = (s) => "[^" + s + "]";
 
 export const begin = "^";
 export const end = "$";
 
-export const ignore = (s) => "[^" + s + "]";
+export const backslash = "\\\\";
 
-
-/* GROUPS */
-
-export const non_capturing_group = (s) => "(?:" + s + ")";
-
-
-/* QUANTIFIERS */
-
-export const any_number_of = (s) => s + "*";
-export const one_or_more = (s) => s + "+";
+export const line_feed = non_capturing_group(any_of("\\\r\\\n", "\\\r", "\\\n"));
