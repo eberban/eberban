901ced9d9e3a5c6e3bc8a91942e0f6fb0d1b9659
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-04-14 13:59:07 +1000

    🧪 Add failing lexer tests for spaces, particles, & roots

---
+++ web/src/scripts/lexer/lexer.test.ts
@@ -0,0 +1,135 @@
+import { describe, expect, test } from "vitest";
+import lex from "./index";
+
+
+describe(`A string is not lexed if it`, () => {
+    test(`is empty`, () => {
+        expect(lex(``)).toStrictEqual([]);
+    });
+    describe(`comprises a single eberban space`, () => {
+        // Note that `[` `]` `-` are not spaces
+        test.for([
+            `q`, `w`, `x`, `y`,
+            ` `, `​`, `👀`, 
+            `~`, `!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `_`, `+`, `=`, `|`, 
+            `(`, `)`,  `{`, `}`, `<`, `>`, `\\`, `/`,
+            `'`, `"`, `\``,
+            `:`, `;`, `,`, `.`, `?`,
+        ])(`%s`, (input) => {
+            expect(lex(input)).toStrictEqual([]);
+        });
+    });
+    describe(`comprises a single repeated eberban space`, () => {
+        test.for([
+            `qq`, `   `, `​​`, `👀👀👀👀👀`, `||||||`,
+            `>>>>>>>`, `''''''''`, `:::::::::`,
+        ])(`%s`, (input) => {
+            expect(lex(input)).toStrictEqual([]);
+        });
+    });
+    describe(`comprises a multitude of eberban spaces`, () => {
+        test.for([
+            `qwwwwqqyyyxxx`, ` q q q q `, `~!@&^#%`, `(*&www👀*)`,
+        ])(`%s`, (input) => {
+            expect(lex(input)).toStrictEqual([]);
+        });
+    });
+});
+
+
+/*
+  DEFINITIONS
+
+  :: vhowel ::
+  
+  A character from the set { h, i , e, a, o, u }. Note that `h` must be between
+  two vowels.
+
+  :: vunit ::
+
+  A single consonant or consonant cluster followed by one or more vhowels. E.g.:
+
+  - sonorant-vunit: sonorant followed by one or more vhowels.
+  - non-sonorant-vunit: non-sonorant consonant followed by one or more vhowels.
+  - medial-pair-vunit: medial pair followed by one or more vhowels.
+
+  Etc.
+*/
+
+
+describe(`An eberban particle is lexed if it`, () => {
+    describe(`comprises a single non-sonorant-vunit`, () => {
+        test.for([`zi`, `mio`, `tiho`, `saeoi`])(`%s`, (input) => {
+            expect(lex(input)).toStrictEqual([{ type: `Particle`, value: input }]);
+        });
+    });
+    describe(`
+        1. begins with at least one vhowel
+        2. follows with any number of sonorant-vunits
+        3. optionally ends with a sonorant
+        `,
+        () => {
+            test.for([`a`, `ahu`, `al`, `anu`, `oie`, `oiu`])(`%s`, (input) => {
+                expect(lex(input)).toStrictEqual([{ type: `Particle`, value: input }]);
+            });
+        },
+    );
+    describe(`
+        1. begins with one or more sonorant-vunits
+        2. optionally ends with a sonorant
+        `,
+        () => {
+            test.for([`ni`, `ra`, `lu`, `nahinel`, `lulu`])(`%s`, (input) => {
+                expect(lex(input)).toStrictEqual([{ type: `Particle`, value: input }]);
+            });
+        },
+    );
+});
+
+
+describe(`An eberban root is lexed if it`, () => {
+    describe(`
+        1. begins with one non-sonorant-vunit
+        2. ends with a sonorant
+        `,
+        () => {
+            test.for([`ber`, `mian`, `vahul`, `boiur`])(`%s`, (input) => {
+                expect(lex(input)).toStrictEqual([{ type: `Root`, value: input }]);
+            });
+        },
+    );
+    describe(`
+        1. begins with one non-sonorant-vunit
+        2. follows with at least one of:
+           a. sonorant-vunit
+           b. medial-pair-vunit
+           c. consonant-triplet-vunit
+        3. optionally ends with a sonorant
+        `,
+        () => {
+            test.for([
+                `dona`, `kitmi`, `makfca`,
+                `toril`, `kagvin`, `siftcen`,
+            ])(`%s`, (input) => {
+                expect(lex(input)).toStrictEqual([{ type: `Root`, value: input }]);
+            });
+        },
+    );
+    describe(`
+        1. begins with one initial-pair-vunit
+        2. follows with any number of:
+           a. sonorant-vunit
+           b. medial-pair-vunit
+           c. consonant-triplet-vunit
+        3. optionally ends with a sonorant
+        `,
+        () => {
+            test.for([
+                `vle`, `jveo`, `ctuhi`, `jbine`, `cfolna`, `vnifkre`,
+                `jvin`, `spoul`, `zgehal`, `bjonil`, `stokman`, `kloptson`,
+            ])(`%s`, (input) => {
+                expect(lex(input)).toStrictEqual([{ type: `Root`, value: input }]);
+            });
+        },
+    );
+});
