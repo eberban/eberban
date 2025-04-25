9db56eeb0ea8afde37cd167a8552688d0c6c5930
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-04-25 10:50:03 +1000

    🚚 Move eberban symbols to separate file

--- web/src/root-generator/logic/random_root.js
+++ web/src/root-generator/logic/random_root.js
@@ -1,5 +1,5 @@
 import { get_random_item }  from "../../scripts/utils";
-import { convert_sequence_to_root, SYMBOLS as S } from "./symbols";
+import { convert_sequence_to_root, SYMBOLS as S } from "./random_symbol";
 
 
 /* ENTRY */
