a0f0d568b2ad9a1bd22de73e7ba90dfad37e10c4
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-04-26 10:59:53 +1000

    ✅ Pass lexing a single eberban particle
    This does not affect the website as the lexer remains unintegrated. This
    PR [continues the test-driven implementation][tdd] of an Eberban lexer.
    
    [tdd]:  https://github.com/eberban/eberban/pull/100
    
    ### New terms
    
    > // A word is lexed into a lexeme if it matches one of its combos.
    > — the code
    
    A `combo` is made up of `combo_item`s which are either shyllables or
    sonorants.
    
    So, in a way, the lexer parses a combo into a lexeme. I found this
    approach reduces regex usage and makes it easier to reason about where
    lexing errors would pop up.

--- web/src/scripts/lexer/index.ts
+++ web/src/scripts/lexer/index.ts
@@ -1,15 +1,10 @@
-const is_alphabetical = (char: string): boolean => {
-    return char === "some alphabet char";
-}
-
-const is_repeat = (char: string, last_char: string): boolean => {
-    const both_are_identical = char === last_char;
-    const both_are_spaces = [
-        char !== "" && last_char !== "",
-        !is_alphabetical(char) && !is_alphabetical(last_char),
-    ].every((bool) => bool);
-    return both_are_identical || both_are_spaces;
-}
+import {
+    Alphabet_char,
+    is_alphabetical,
+    is_repeat,
+} from "./types_and_preds";
+import build_combo from "./combo";
+import { any_number_of, begin, end, non_capturing_group, one_or_more, optional } from "../regex";
 
 interface Lexeme {
     readonly type: string,
@@ -19,18 +14,55 @@ interface Lexeme {
 const SPACE: Lexeme = { type: "Space", value: "" };
 
 
+/* ENTRY */
 export default function lex(input: string): Lexeme[] {
     const lexemes: Lexeme[] = [];
+    let possible_word: Alphabet_char[] = [];
     let last_char = "";
     for (const char of input) {
         if (is_repeat(char, last_char)) {
             continue;
         } else if (is_alphabetical(char)) {
-            ;
+            possible_word.push(char);
         } else {
             lexemes.push(SPACE);
+            lexemes.push(...lex_possible_word(possible_word))
+            possible_word = [];
         }
         last_char = char;
     }
+    if (possible_word.length > 0) {
+        lexemes.push(...lex_possible_word(possible_word))
+    }
     return lexemes;
 };
+
+
+function lex_possible_word(possible_word: Alphabet_char[]): Lexeme[] {
+    const lexemes: Lexeme[] = [];
+    // A word is lexed into a lexeme if it matches one of its combos.
+    const particle_combos = [
+        "non-sonorant-shyllable",
+        (
+            "mahul" +
+            any_number_of(non_capturing_group("sonorant-shyllable")) +
+            optional(non_capturing_group("sonorant"))
+        ),
+        (
+            one_or_more(non_capturing_group("sonorant-shyllable")) +
+            optional(non_capturing_group("sonorant"))
+        ),
+    ]
+    const possible_word_combo = build_combo(possible_word);
+    const combo_string = possible_word_combo.map((c) => c.type).join("");
+    for (const regex_string of particle_combos) {
+        const regex = new RegExp(begin + regex_string + end);
+        if (regex.test(combo_string)) {
+            lexemes.push({ 
+                type: "Particle",
+                value: possible_word_combo.map((c) => c.get_content()).join(""),
+            });
+        }
+    }
+    return lexemes;
+}
