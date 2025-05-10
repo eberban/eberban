9dd0621442bd7338de26f238bdea53276840f34b
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-05-10 10:24:31 +1000

    ♻️ Implement react-string-replace ourselves

--- web/src/dictionary/markup_text.jsx
+++ web/src/dictionary/markup_text.jsx
@@ -1,4 +1,4 @@
-import reactStringReplace from "react-string-replace";
+import markup_to_jsx_child from "./markup_to_jsx_child";
 import {
     any,
     any_of,
@@ -9,6 +9,7 @@ import {
     group,
     line_feed,
     non_capturing_group,
+    not_in_set,
     one_or_more,
     optional,
     set,
@@ -17,18 +18,17 @@ import {
 } from "../scripts/regex";
 
 
-/* HELPERS */
-
-
-const remove_delimiters = (text) => text.substring(1, text.length - 1);
-
 function DictLink({ children }) {
     return <a href={`#${children}`}>{children}</a>;
 }
 
 
 /* 
-    Kit :: () -> { regex: RegExp(), replacer: string -> JSX }
+    Kit :: () -> { 
+        keep_children_as_string: boolean?
+        regex_string: string
+        replacer: Replacer
+    }
 
     Kits are how we extend markup syntax. They're written as functions for
     readability.
@@ -37,7 +37,7 @@ function DictLink({ children }) {
 
 function break_kit() {
     return {
-        regex: new RegExp(group(backslash + line_feed), "g"),
+        regex_string: group(backslash + line_feed),
         replacer: () => <br />,
     };
 }
@@ -45,93 +45,84 @@ function break_kit() {
 function bold_kit() {
     const bolded = (s) => "__" + s + "__";
     return {
-        regex: new RegExp(group(bolded(fewest_positive_number_of(any))), "g"),
-        replacer: (s) => <strong>{remove_delimiters(remove_delimiters(s))}</strong>,
+        regex_string: bolded(group(fewest_positive_number_of(not_in_set("_")))),
+        replacer: (_, content) => <strong>{content}</strong>,
     };
 }
 
 function italics_kit() {
     const in_italics = (s) => "_" + s + "_";
     return {
-        regex: new RegExp(group(in_italics(fewest_positive_number_of(any))), "g"),
-        replacer: (s) => <em>{remove_delimiters(s)}</em>,
+        regex_string: in_italics(group(fewest_positive_number_of(not_in_set("_")))),
+        replacer: (_, content) => <em>{content}</em>,
     };
 }
 
 function definition_quote_kit() {
     const in_quote = (s) => "`" + s + "`";
     return {
-        regex: new RegExp(group(in_quote(fewest_positive_number_of(any))), "g"),
-        replacer: (s) => <code>{remove_delimiters(s)}</code>,
+        keep_children_as_string: true,
+        regex_string: in_quote(group(fewest_positive_number_of(any))),
+        replacer: (_, content) => <code>{content}</code>,
     };
 }
 
 function eberban_quote_kit() {
-    function whole() {
-        const in_quote = (s) => "{" + s + "}";
-        return {
-            regex: new RegExp(group(in_quote(fewest_positive_number_of(any))), "g"),
-            replacer: (string) => {
-                const kit = content_kit();
-                let content = remove_delimiters(string);
-                content = reactStringReplace(content, kit.regex, kit.replacer);
-                content = reactStringReplace(content, "!", () => "");
-                return <span class="ebb-quote">{content}</span>;
-            },
-        };
+    const in_quote = (s) => "{" + s + "}";
+    return {
+        keep_children_as_string: true,
+        regex_string: in_quote(group(fewest_positive_number_of(any))),
+        replacer: (_, content) => {
+            const { regex_string, replacer } = content_kit();
+            let rendered_content = markup_to_jsx_child(content, regex_string, replacer);
+            rendered_content = markup_to_jsx_child(rendered_content, "!", () => "");
+            return <span class="ebb-quote">{rendered_content}</span>;
+        },  
     };
     function content_kit() {
         const as_one_link = (s) => "<" + s + ">";
-        const simple_word_link = 
-            does_not_begin_with("!" + any_number_of(word_char)) + one_or_more(word_char);
-        const compound_word_link = as_one_link(non_capturing_group(
-            fewest_positive_number_of(
-                non_capturing_group(one_or_more(word_char) + optional(space)),
-            ),
+        const simple_word_link = group(
+            does_not_begin_with("!" + any_number_of(word_char)) +
+            one_or_more(word_char)
+        );  
+        const compound_word_link = as_one_link(group(
+            fewest_positive_number_of(non_capturing_group(
+                one_or_more(word_char) + optional(space)
+            ))
         ));
         return {
-            regex: new RegExp(group(any_of(simple_word_link, compound_word_link)), "g"),
-            replacer: (string) => {
-                let output = string;
-                if (new RegExp(compound_word_link).test(output)) {
-                    output = remove_delimiters(output);
-                }
-                return <DictLink>{output}</DictLink>;
-            }
+            regex_string: any_of(simple_word_link, compound_word_link),
+            replacer: (_, link) => <DictLink>{link}</DictLink>,
         };
-    };
-    return whole();
+    }
 }
 
 function place_kit() {
-    function whole() {
-        const in_brackets = (s) => "\\[" + s + "\\]";
-        const place = set("E", "A", "O", "U");
-        const arg = non_capturing_group(":" + fewest_positive_number_of(any));
+    const in_brackets = (s) => "\\[" + (s) + "\\]";
+    const place = group(set("E", "A", "O", "U"));
+    const arg = group(":" + fewest_positive_number_of(any));
+    return {
+        regex_string: in_brackets(place + optional(arg)),
+        replacer: (_, place, arg) => {
+            const { regex_string, replacer } = arg_kit();
+            // arg is a capture group that's optional. It won't always show up
+            // in the captured strings of replacer.
+            const arg_with_links = markup_to_jsx_child(arg ?? "", regex_string, replacer);
+            return (
+                <span class="label label-info place">
+                    <span class="hidden">{"["}</span>
+                    {place}{arg_with_links}
+                    <span class="hidden">{"]"}</span>
+                </span>
+            )
+        }
+    }
+    function arg_kit() {
         return {
-            regex: new RegExp(group(in_brackets(place + optional(arg))), "g"),
-            replacer: (string) => {
-                const [_, left_bracket, content, right_bracket] = string.match(
-                    new RegExp(group("\\[") + group(one_or_more(any)) + group("\\]"))
-                );
-                const kit = content_kit();
-                return (
-                    <span class="label label-info place">
-                        <span class="hidden">{left_bracket}</span>
-                        {reactStringReplace(content, kit.regex, kit.replacer)}
-                        <span class="hidden">{right_bracket}</span>
-                    </span>
-                );
-            },
-        };
-    };
-    function content_kit() {
-        return {
-            regex: new RegExp(group(word_char + one_or_more(word_char)), "g"),
+            regex_string: group(word_char + one_or_more(word_char)),
             replacer: (s) => <DictLink>{s}</DictLink>,
         };
-    };
-    return whole();
+    }
 }
 
 
@@ -142,15 +133,20 @@ export function markup_inline(text) {
     let output = text;
     const markup_kits = [
         break_kit,
-        bold_kit, // bold before italics otherwise ___s___ ->  _<em>{s}</em>_
+        bold_kit,
         italics_kit,
         definition_quote_kit,
         eberban_quote_kit,
         place_kit,
     ];
     for (const kit of markup_kits) {
-        const { regex, replacer } = kit();
-        output = reactStringReplace(output, regex, replacer);
+        const { keep_children_as_string, regex_string, replacer } = kit();
+        output = markup_to_jsx_child(
+            output,
+            regex_string,
+            replacer,
+            keep_children_as_string
+        );
     }
     return output;
 }
---
+++ web/src/dictionary/markup_to_jsx_child.ts
@@ -0,0 +1,111 @@
+import react_htmr from "htmr";
+import { JSX, VNode } from "preact";
+import render_to_string from "preact-render-to-string";
+import type { JSX_Child, Replacer } from "./markup_types";
+
+/* WORKAROUND */
+
+const htmr = (input: string) => {
+    // htmr deals in React.ReactNode
+    // Preact deals in VNode
+    // So we cast to VNode as that's what we mostly use.
+    return react_htmr(input) as VNode;
+}
+
+/* TYPES */
+
+
+type Replacement = {
+    begin_index: number,
+    end_index: number,
+    jsx: JSX.Element,
+};
+
+// string.matchAll() requires a global regex.
+type Global_Regex = RegExp;
+
+
+/* ENTRY */
+
+
+export default function markup_to_jsx_child(
+    input: JSX_Child,
+    regex_string: string,
+    replacer: Replacer,
+    keep_children_as_string: boolean = false,
+) {
+    const string_input = jsx_children_to_string(input);
+
+    // We use a separate regex to test whether we need to make_replacements().
+    // This is because using regex.test() on a global regex advances its lastIndex
+    // and that will produce inconsistent behaviour.
+    if (!new RegExp(regex_string).test(string_input)) {
+        return input;
+    }
+    const replacements = make_replacements(
+        string_input,
+        new RegExp(regex_string, "g"),
+        replacer,
+        keep_children_as_string
+    );
+    return replace_string_with_jsx(string_input, replacements);
+}
+
+function jsx_children_to_string(input: JSX_Child): string {
+    if (typeof input === "string") {
+            return input;
+        }
+    return render_to_string(input);
+}
+
+function make_replacements(
+    input: string,
+    regex: Global_Regex,
+    replacer: Replacer,
+    keep_children_as_string: boolean
+) {
+    if (input.length === 0) {
+        return [];
+    }
+    const replacements: Replacement[] = [];
+    const matched = input.matchAll(regex);
+    for (const match_array of matched) {
+        const whole_string = match_array[0];
+        // Not every capture group of the regex will match. We filter out failed
+        // captures so that the replacer() only gets the captured strings.
+        const captured_strings = match_array.slice(1).filter((x) => x !== undefined);
+        const jsx = (() => {
+            if (keep_children_as_string) {
+                return replacer(whole_string, ...captured_strings);
+            }
+            return replacer(htmr(whole_string), ...captured_strings.map((s) => htmr(s)));
+        })();
+        replacements.push({
+            begin_index: match_array.index,
+            end_index: match_array.index + (whole_string.length - 1),
+            jsx,
+        });        
+    }
+    return replacements;
+}
+
+function replace_string_with_jsx(input: string, replacements: Replacement[]): JSX_Child {
+    // Considered alternative: returning JSX_Child[].
+    // This doesn't work. We already need to have our input as string so that we
+    // can perform regex. This means we'll need to htmr() it again. htmr(),
+    // however, trims its string input. So if we pass it childs of a JSX_Child[],
+    // then we lose spaces on the rendered JSX.  
+    let output = "";
+    let last_index = 0;
+    for (const replacement of replacements) {
+        if (last_index < replacement.begin_index) {
+            output += input.substring(last_index, replacement.begin_index);
+        }
+        output += render_to_string(replacement.jsx)
+        last_index = replacement.end_index + 1;
+    }
+    if (last_index <= (input.length - 1)) {
+        output += input.substring(last_index);
+    }
+    return htmr(output);
+}
