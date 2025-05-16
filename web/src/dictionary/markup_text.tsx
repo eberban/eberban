3999e751c783674a92b82230f48238e48bbda799
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-05-16 10:08:38 +1000

    🚚 markup_text.jsx -> markup_text.tsx

--- web/src/dictionary/markup_text.tsx
+++ web/src/dictionary/markup_text.tsx
@@ -1,3 +1,4 @@
+import type { JSX_Child, Replacer } from "./markup_types";
 import markup_to_jsx_child from "./markup_to_jsx_child";
 import {
     any,
@@ -18,69 +19,72 @@ import {
 } from "../scripts/regex";
 
 
-function DictLink({ children }) {
+function DictLink({ children }: { children: string}) {
     return <a href={`#${children}`}>{children}</a>;
 }
 
 
-/* 
-    Kit :: () -> { 
-        keep_children_as_string: boolean?
-        regex_string: string
-        replacer: Replacer
-    }
-
-    Kits are how we extend markup syntax. They're written as functions for
-    readability.
-*/
+/* TYPES */
 
 
-function break_kit() {
+// Kits are how we extend markup syntax. 
+type Kit = {
+    // If this is true, we cast the parameters of Replacer to string.
+    keep_children_as_string?: boolean,
+    regex_string: string,
+    replacer: Replacer,
+}
+
+
+/* KITS -- written as functions for readability */
+
+
+function break_kit(): Kit {
     return {
         regex_string: group(backslash + line_feed),
         replacer: () => <br />,
     };
 }
 
-function bold_kit() {
-    const bolded = (s) => "__" + s + "__";
+function bold_kit(): Kit {
+    const bolded = (s: string) => "__" + s + "__";
     return {
         regex_string: bolded(group(fewest_positive_number_of(not_in_set("_")))),
         replacer: (_, content) => <strong>{content}</strong>,
     };
 }
 
-function italics_kit() {
-    const in_italics = (s) => "_" + s + "_";
+function italics_kit(): Kit {
+    const in_italics = (s: string) => "_" + s + "_";
     return {
         regex_string: in_italics(group(fewest_positive_number_of(not_in_set("_")))),
         replacer: (_, content) => <em>{content}</em>,
     };
 }
 
-function definition_quote_kit() {
-    const in_quote = (s) => "`" + s + "`";
+function definition_quote_kit(): Kit {
+    const in_quote = (s: string) => "`" + s + "`";
     return {
         keep_children_as_string: true,
         regex_string: in_quote(group(fewest_positive_number_of(any))),
-        replacer: (_, content) => <code>{content}</code>,
+        replacer: (_, content) => <code>{content as string}</code>,
     };
 }
 
-function eberban_quote_kit() {
-    const in_quote = (s) => "{" + s + "}";
+function eberban_quote_kit(): Kit {
+    const in_quote = (s: string) => "{" + s + "}";
     return {
         keep_children_as_string: true,
         regex_string: in_quote(group(fewest_positive_number_of(any))),
         replacer: (_, content) => {
             const { regex_string, replacer } = content_kit();
             let rendered_content = markup_to_jsx_child(content, regex_string, replacer);
-            rendered_content = markup_to_jsx_child(rendered_content, "!", () => "");
+            rendered_content = markup_to_jsx_child(rendered_content, "!", () => <></>);
             return <span class="ebb-quote">{rendered_content}</span>;
         },  
     };
-    function content_kit() {
-        const as_one_link = (s) => "<" + s + ">";
+    function content_kit(): Kit {
+        const as_one_link = (s: string) => "<" + s + ">";
         const simple_word_link = group(
             does_not_begin_with("!" + any_number_of(word_char)) +
             one_or_more(word_char)
@@ -92,13 +96,13 @@ function eberban_quote_kit() {
         ));
         return {
             regex_string: any_of(simple_word_link, compound_word_link),
-            replacer: (_, link) => <DictLink>{link}</DictLink>,
+            replacer: (_, link) => <DictLink>{link as string}</DictLink>,
         };
     }
 }
 
-function place_kit() {
-    const in_brackets = (s) => "\\[" + (s) + "\\]";
+function place_kit(): Kit {
+    const in_brackets = (s: string) => "\\[" + (s) + "\\]";
     const place = group(set("E", "A", "O", "U"));
     const arg = group(":" + fewest_positive_number_of(any));
     return {
@@ -117,10 +121,10 @@ function place_kit() {
             )
         }
     }
-    function arg_kit() {
+    function arg_kit(): Kit {
         return {
             regex_string: group(word_char + one_or_more(word_char)),
-            replacer: (s) => <DictLink>{s}</DictLink>,
+            replacer: (s) => <DictLink>{s as string}</DictLink>,
         };
     }
 }
@@ -129,8 +133,8 @@ function place_kit() {
 /* ENTRY */
 
 
-export function markup_inline(text) {
-    let output = text;
+export function markup_inline(text: string) {
+    let output: JSX_Child = text;
     const markup_kits = [
         break_kit,
         bold_kit,
@@ -151,7 +155,7 @@ export function markup_inline(text) {
     return output;
 }
 
-export function markup_block(text) {
+export function markup_block(text: string) {
     return text
         .split(new RegExp(group(line_feed + line_feed), "g"))
         .map((content) => <p>{markup_inline(content)}</p>);    
