4ff6856dd2d171145497c6fff457480a96a1a55a
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2025-11-02 14:39:53 +1100

    👔 Deem consecutive confusables to be repeats
    Note: the set of confusable characters does not
    include whitespace. I'm not sure why, but that's how it is.
    
    We'll now be checking for look-alike characters and characters of
    different cases. However, because we're not lexing words, no tests have
    been written for them right now.

--- web/src/scripts/lexer/index.ts
+++ web/src/scripts/lexer/index.ts
@@ -1,14 +1,22 @@
+import deconfuse from "confusables"; 
+
 const is_alphabetical = (char: string): boolean => {
     return char === "some alphabet char";
 }
 
+// TODO when lexing foreign quotes, do not check for repeats
+// TODO when lexing words like particles, write tests for repeated characters
 const is_repeat = (char: string, last_char: string): boolean => {
-    const both_are_non_empty = char.length > 0 && last_char.length > 0;
-    const both_are_identical = char === last_char;
-    const both_are_spaces = !is_alphabetical(char) && !is_alphabetical(last_char);
-    // A space is defined by NOT being alphabetical, so we must ensure that they
-    // are non-empty characters as well. Otherwise it would be incorrectly lexed.
-    return both_are_non_empty && (both_are_identical || both_are_spaces);
+     const both_are_non_empty = char.length > 0 && last_char.length > 0;
+     // deconfuse tries to replace invisible characters as empty strings.
+     // We're choosing to count these as spaces so the non-empty character
+     // check must be separate.
+     const both_are_identical = deconfuse(char.toLowerCase()) === deconfuse(last_char.toLowerCase());
+     const both_are_spaces = !is_alphabetical(char) && !is_alphabetical(last_char);
+     // A space is defined by NOT being alphabetical. But this alone would
+     // include empty characters, so the non-empty character check must be
+     // separate.
+     return both_are_non_empty && (both_are_identical || both_are_spaces);
 }
 
 interface Lexeme {
--- web/src/scripts/lexer/lexer.test.ts
+++ web/src/scripts/lexer/lexer.test.ts
@@ -28,6 +28,11 @@ describe(`A single eberban space is lexed if the inputted string`, () => {
             expect(lex(input)).toStrictEqual([{type: "Space", value: "" }]);
         });
     });
+    describe(`comprises multiple different whitespace characters`, () => {
+        test.for([` ​​`, `   `, `  　  `])(`%s`, (input) => {
+            expect(lex(input)).toStrictEqual([{type: "Space", value: "" }]);
+        });
+    })
     describe(`begins with hyphen(s) and ends with eberban space(s)`, () => {
         test.for([`- `, `--: `, `---)...`])(`%s`, (input) => {
             expect(lex(input)).toStrictEqual([{type: "Space", value: "" }]);
